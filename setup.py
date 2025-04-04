cdc579fe17481c7bb61295bd284b5bf2de23ac10
VENKATESH <90443397+Venkatvicky@users.noreply.github.com>
2025-04-04 17:38:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my-sample-pkg',
-    version='0.0.1',
-    author='Your Name',
-    author_email='you@example.com',
-    description='A sample Python package for Nexus pipeline demo',
+    name='sample-python-pkg',
+    version='0.1.0',
+    author='Venkat',
+    author_email='venkat@example.com',
+    description='A simple Python package to demo Jenkins + Nexus',
     packages=find_packages(),
-    install_requires=[],  # or ['requests', ...]
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-    ],
     python_requires='>=3.6',
 )
